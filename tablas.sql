428d1f4e0d8d099aca965c85bb3bfb05456e2d3b
Ckancord33 <niccord33@gmail.com>
2025-05-17 21:47:33 -0500

    Documentacion tablas

--- tablas.sql
+++ tablas.sql
@@ -1,9 +1,11 @@
+--Tabla estudiante
 CREATE TABLE estudiante(
   eCodigo SERIAL PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL
 );
  
+--Tabla asignatura
 CREATE TABLE asignatura(
   aCodigo SERIAL PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL,
@@ -11,6 +13,7 @@ CREATE TABLE asignatura(
   CHECK(nHoras > 0)
 );
 
+--Tabla profesor
 CREATE TABLE profesor(
   pCodigo SERIAL PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL,
@@ -18,6 +21,7 @@ CREATE TABLE profesor(
   CHECK (correo LIKE '%_@_%.__%')
 );
 
+--Tabla aula
 CREATE TAble aula(
   num INT PRIMARY KEY,
   capacidad INT NOT NULL,
@@ -27,6 +31,7 @@ CREATE TAble aula(
   CHECK (capacidad > 0 AND piso >= 0)
 );
 
+--Tabla curso
 CREATE TABLE curso(
   id SERIAL PRIMARY KEY,
   fechaInicio DATE NOT NULL,
@@ -40,6 +45,7 @@ CREATE TABLE curso(
   	ON DELETE SET NULL
 );
 
+--Tabla horario, atributo multivaluado de curso
 CREATE TABLE horario(
   idhorario SERIAL,
   id INT,
@@ -52,6 +58,7 @@ CREATE TABLE horario(
   	ON DELETE CASCADE
 );
 
+--Tabla aulaCurso, relacion entre aula y curso "asigna" en el MER
 CREATE TABLE aulaCurso(
   num INT,
   id INT,
@@ -62,6 +69,7 @@ CREATE TABLE aulaCurso(
   	ON DELETE CASCADE
 );
 
+--Tabla estudianteAsignatura, relacion entre estudiante y asignatura "Matricula" en el MER
 CREATE TABLE estudianteAsignatura(
   eCodigo INT,
   aCodigo INT,
@@ -71,7 +79,8 @@ CREATE TABLE estudianteAsignatura(
   FOREIGN KEY(aCodigo) REFERENCES asignatura(aCodigo)
   	ON DELETE CASCADE
 );
-  
+
+--Tabla correoEstudiante, atributo multivaluado de estudiante para los correos
 CREATE TABLE correoEstudiante(
   direccionElectronica VARCHAR(50) CHECK (direccionElectronica LIKE '%_@_%.__%'),
   eCodigo INT,
